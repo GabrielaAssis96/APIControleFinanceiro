2e09e14f5d93158b47b3ca7515c26bc6282c34a7
Gabriela Assis <gabriela_assis96@hotmail.com>
2022-07-22 21:49:33 -0300

    Criacao de estrutua do projeto
    Criacao do projeto utilizando gradle

---
+++ src/main/kotlin/br/com/gabi/ControleFinanceiro/ControleFinanceiroApplication.kt
@@ -0,0 +1,11 @@
+package br.com.gabi.ControleFinanceiro
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ControleFinanceiroApplication
+
+fun main(args: Array<String>) {
+	runApplication<ControleFinanceiroApplication>(*args)
+}
